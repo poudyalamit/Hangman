641e7191bc641e9656fac916adbb6d5081f61a1f
poudyalamit <poudyalamit20@gmail.com>
2023-05-05 00:14:49 +0545

    word display done with css

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,16 @@
-export function HangmanWord(){
-    return <h1></h1>
+export function HangmanWord() {
+
+    const word = 'test';
+    const guessedLetters = ["t", "s", "y"]
+    return (<div style={{ display: "flex", gap: ".25rem", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace" }}>
+        {word.split("").map((letter, index) => (
+            <span style={{ borderBottom: ".5rem solid black" }} key={index}>
+                <span style={{
+                    visibility: guessedLetters.includes(letter)
+                        ? "visible" : "hidden"
+                }}>{letter}</span>
+            </span>
+        ))}
+    </div>
+    )
 }
